61dba33b62bad0a1b1beacb41fa81b9173cc899a
Tisls <105556356+Tisls@users.noreply.github.com>
2022-05-29 01:07:57 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+print("Результат:", 7, 15, sep=";", end="!\n")
+print("alala \" d\tf;f\nd")
+print("aaa\t aaa")
+print("\\")
+print(5 + 4)
+print(6 * 5)
+print(6 / 5)
+print(6 - 2)
+print("resultat chisla:", 5 * 4 )# помножити
+print("resultat kvadrata",5 ** 2)# піднести до степення
+print("r:", min(1,2,3,4,5,6))# знаходження мінімального числа
+print("max:", max(1,2,3,4,5,6))# знаходження максимального числа
+print("abs:",abs(-5)) # модуль числа
+print("kvadrat",pow(5,3)) #піднесення до степення числа
+print("round", round(5 / 3)) #о
+input("chislo=>")
+print("pgfpgfdfdfdfd gfgfgfgfg)
