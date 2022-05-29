914e13ef4f1bac17b9a9d12fff9df0b0d8b0e92e
Tisls <105556356+Tisls@users.noreply.github.com>
2022-05-29 21:09:39 +0300

    1

---
+++ 5less.py
@@ -0,0 +1,10 @@
+user_data = int(input("Number"))
+isHappy = False
+
+if isHappy and user_data == 6:
+    print("User is happy")
+elif user_data != 5:
+    print("It is not a 5")
+else:
+    print("It is a 5")
+
