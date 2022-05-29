fd51d4bfbdf90b304f7393ddb489d2668fc76fa5
Tisls <105556356+Tisls@users.noreply.github.com>
2022-05-29 20:34:27 +0300

    1

---
+++ naa.py
@@ -0,0 +1,18 @@
+x = 5
+del x
+
+x = 7
+print(x)
+y = 4.52342342
+print(y)
+word = ("результат")
+print(word, x)
+
+
+zxc = True
+print(zxc)
+print(str(x) + word)
+
+xcz = 5
+cxz = "5"
+print(word + str(int(cxz) + xcz))
---
+++ prog.py
@@ -0,0 +1,19 @@
+button1 = int(input("First number=>"))
+
+button2 = int(input("Second number=>"))
+
+button1 += 5
+button1 *= 5
+button2 /= 5
+
+print("Result=>", button1 + button2)
+print("Result=>", button1 * button2)
+print("Result=>", button1 - button2)
+print("Result=>", button1 / button2)
+print("Result=>", button1 ** button2)
+print("Result=>", button1 // button2)
+
+world = "Gi"
+print(world * 4)
+
+world = True
